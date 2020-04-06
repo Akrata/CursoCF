02e3677225b48022f65de0715545a42328d872a1
Puagh <pablofeijo181@gmail.com>
2020-04-06 20:21:17 -0300

    funciona?

---
+++ cursocf.py
@@ -0,0 +1,33 @@
+
+"""
+base =float(input("indique la medida de la base\n"))
+altura =float(input("indique la medida de la altura\n"))
+resultado = base * altura /2
+
+print ("el resultado es = "+ str(resultado))
+"""
+
+
+
+
+"""
+precio_dolar = 45
+
+cantidad_dolares = float(input("Cuantos Dolares desea cambiar a pesos?\n"))
+
+total = str(cantidad_dolares * precio_dolar)
+print("Cantidad de Pesos Uruguayos a retirar = $" + total )
+
+"""
+
+
+
+
+
+"""grados_c = float(input("ingrese el valor de grados celcius a convertir\n"))
+grados_f = str((grados_c*9/5)+32)
+
+print(str(grados_c) + " grados C son" + grados_f + "grados F")
+""""
+
+
