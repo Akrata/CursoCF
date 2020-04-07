29428d8c91a8476d9c7bd103a9ba082f92b12c0a
Puagh <pablofeijo181@gmail.com>
2020-04-07 07:13:46 -0300

    por finalizar los trabajos basicos

--- cursocf.py
+++ cursocf.py
@@ -29,6 +29,10 @@ grados_f = str((grados_c*9/5)+32)
 
 print(str(grados_c) + " grados C son" + grados_f + "grados F")
 """
+
+
+
+"""
 lustro = 5
 año = 365
 dia = 24
@@ -36,3 +40,40 @@ hora = 60
 minuto = 60
 
 print(" un lustro tiene "+ str(minuto * hora*dia*año*lustro ) + "segundos")
+
+"""
+
+
+"""
+distancia_sol_marte = 227940000
+
+velocidad_luz = 300000
+
+print(str(distancia_sol_marte*velocidad_luz))
+"""
+
+import math
+
+# nv = 1c / 2PI*r
+
+"""
+longitud = 100000
+radio = 25
+PI = math.pi
+
+print ("Vueltas:"+str(longitud/(2*PI*radio)))
+
+"""
+"""VERRRRRR
+grados = 22
+altura = float(20)
+
+tangente = math.tan(grados)
+print (tangente)
+print (str((altura)/tangente))
+print (20 / tangente)
+"""
+
+
+
+
